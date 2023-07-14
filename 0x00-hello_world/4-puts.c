fbd82253beaef23a8d8b1914b5da3c9073bd34ac
KingsleyOC <KingsleyOC@gmail.com>
2023-07-14 14:13:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@ include<stdio.h>
  */
 int main (void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	
 	return (0);
 }
