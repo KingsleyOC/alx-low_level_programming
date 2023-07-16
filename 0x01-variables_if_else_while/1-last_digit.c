ca2b23ab78e095a66dcebdfdb5056d806bffaef5
KingsleyOC <KingsleyOC@gmail.com>
2023-07-16 16:58:00 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
   *main - Entry point
   *Description: 'get the last digit of a number'
-  *Return: always 0 
+  *Return: Always 0 
   */
 int main(void)
 {
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,18 @@
  */
 int main(void)
 {
-	int n = 65;
+	int n = 97;
+	int m = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
 		n++;
 	}
-	while (n <= 90)
+	while (m <= 90)
 	{
-		putchar(n);
-		n++;
+		putchar(m);
+		m++;
 	}
 	putchar('\n');
 	return (0);
