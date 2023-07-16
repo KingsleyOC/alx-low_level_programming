0ea5fdc63d0da810ff98f4c8e062daf5e8abc47e
KingsleyOC <KingsleyOC@gmail.com>
2023-07-16 10:50:54 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *Description: 'print numbers of base 10'
- *Return: Always 0
- */
+  *main - Entry point
+  *Description: 'print numbers of base 10'
+  *Return: Always 0
+  */
 int main(void)
 {
 	int n;
@@ -12,6 +12,6 @@ int main(void)
 	{
 		printf("%d", n);
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
