b5b1d56953f3470431633a8b6d9665a1492a0101
Okechukwu Kingsley <kingsleyokechukwu890@gmail.com>
2024-03-13 15:01:15 -0700

    1-binary

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,29 +1,9 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef SEARCH_H
+#define SEARCH_H
 
-#include <stddef.h>  /* Include necessary standard headers */
+#include <stdio.h>
 
-/* Definition for listint_t */
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
-} listint_t;
-
-/* Definition for skiplist_t */
-typedef struct skiplist_s {
-    int n;
-    struct skiplist_s *express;
-    struct skiplist_s *next;
-} skiplist_t;
-
-/* Function prototypes */
+/* Prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
-
-#endif /* SEARCH_ALGOS_H */
+#endif
