f5bd4d55e8c67026c692a56799a5a5641bcc5a5c
Okechukwu Kingsley <kingsleyokechukwu890@gmail.com>
2024-03-16 09:49:49 -0700

    104-advanced_binary.c

--- 0x1E-search_algorithms/102-main.c
+++ 0x1E-search_algorithms/102-main.c
@@ -1,7 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "search_algos.h"
-
-/* Function prototype for interpolation_search */
-int interpolation_search(int *array, size_t size, int value);
 
 /**
  * main - Entry point
@@ -10,11 +9,13 @@ int interpolation_search(int *array, size_t size, int value);
  */
 int main(void)
 {
-    int array[] = { /* your array values here */ };
+    int array[] = {
+        0, 0, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 9, 9
+    };
     size_t size = sizeof(array) / sizeof(array[0]);
 
     printf("Found %d at index: %d\n\n", 3, interpolation_search(array, size, 3));
-    /* additional code for testing other scenarios */
-
+    printf("Found %d at index: %d\n\n", 7, interpolation_search(array, size, 7));
+    printf("Found %d at index: %d\n", 999, interpolation_search(array, size, 999));
     return (EXIT_SUCCESS);
 }
